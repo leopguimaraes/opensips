8f6a127ddfccc8e7ff10aa565420983a06df688d
Leonardo Guimaraes <leopguimaraes@gmail.com>
2017-09-12 18:09:38 -0300

    Meu manifest default.pp

--- default.pp
+++ default.pp
@@ -1,1 +1,18 @@
-../default.pp
+$pacotesopensips = ['gcc', 'gcc-c++', 'epel-release', 'bison', 'flex', 'zlib-devel', 'openssl-devel', 'mariadb-devel', 'mariadb', 'subversion', 'pcre-devel']
+user {'root':
+  ensure => present,
+  password => '$1$4/enXW3g$xiKNkxhlO5NEtbGJ7Td6q.'
+}
+package { 'vim-enhanced':
+  ensure => installed
+}
+package { $pacotesopensips:
+  ensure => installed
+}
+exec { "yum install -y http://yum.opensips.org/2.2/releases/el/7/x86_64/opensips-yum-releases-2.2-3.el7.noarch.rpm":
+  cwd => "/var/tmp",
+  path => ["/usr/bin", "/usr/sbin"]
+}
+package { 'opensips':
+  ensure => installed,
+}
